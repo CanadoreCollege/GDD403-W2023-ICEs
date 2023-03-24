a712bbe9ecca8ebf1f84238ae0f98c417686f9d4
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2023-03-24 11:47:17 -0400

    added Project files for ICE7

---
+++ GDD403-W2023-ICE7/Assets/[Scripts]/BulletController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BulletController : MonoBehaviour
+{
+    public Vector3 direction;
+    public float speed;
+
+    private BulletManager bulletManager;
+
+    void Start()
+    {
+        bulletManager = FindObjectOfType<BulletManager>();
+    }
+
+    public void Activate()
+    {
+        Invoke("DestroyYourSelf", 2.0f);
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+    public void DestroyYourSelf()
+    {
+        if (gameObject.activeInHierarchy)
+        {
+            bulletManager.ReturnBullet(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DestroyYourSelf();
+    }
+
+
+}
