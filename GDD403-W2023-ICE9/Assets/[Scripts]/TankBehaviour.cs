813e7f8d96c21770b3d4670b619f08e1c08d0c04
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2023-04-14 11:01:25 -0400

    added ICE9 Project Files

---
+++ GDD403-W2023-ICE9/Assets/[Scripts]/TankBehaviour.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankBehaviour : MonoBehaviour
+{
+    [Header("Movement Properties")]
+    public float rotationRate;
+    public float speed;
+    public Vector3 direction;
+
+    [Header("Health System")] 
+    public HealthSystem health;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveTankBody();
+    }
+
+    private void MoveTankBody()
+    {
+        var rotationAngle = Input.GetAxisRaw("Horizontal") * rotationRate * Time.deltaTime * -1.0f;
+        var movementRate = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+
+        transform.Rotate(Vector3.forward, rotationAngle);
+        var radiansAngle = transform.localEulerAngles.z * Mathf.Deg2Rad;
+        direction = new Vector3((float)Mathf.Cos(radiansAngle), (float)Mathf.Sin(radiansAngle), 0.0f);
+
+        transform.position += direction * movementRate;
+    }
+}
